16785d318c6666d65c45821062340f4874a78886
12aura <auraannu@gmail.com>
2025-05-23 15:51:34 +0530

    Added the code for the User.js file

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,30 @@
+import User from "../models/User.js";
+
 export async function signup(req, res){
-    res.send("Sighup Route");
+    const {email, password, fullName} = req.body;
+
+    try{
+        if(!email || !password || !fullName)
+        {
+            return res.status(400).json({message: "All fields are required"});
+        }
+        if(password.length < 6)
+        {
+            return res.status(400).json({message: "Password must be at least 6 characters"});
+        }
+
+        //john@gmail
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: "Invalid email format" });
+        }
+
+        const existingUser = await User.findOne({email});
+    }catch(error)
+    {
+
+    }
 }
 
 export async function login(req, res){
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,6 +1,80 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt.js"
 
-const userSchema = new mongoose.Schema({}, {timestamps:true});
+const userSchema = new mongoose.Schema({
+    fullName:{
+        type: String, 
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type:String,
+        required: true,
+        minlength:6
+    },
+    bio:
+    {
+        type:String,
+        default:"",
+    },
+    profilePic:
+    {
+        type:String,
+        default:"",
+    },
+    nativeLanguage:
+    {
+        type:String,
+        default:"",
+    },
+    learningLanguage:
+    {
+        type:String,
+        default:"",
+    },
+    location:
+    {
+        type:String,
+        default:"",
+    },
+    isOnboarded:
+    {
+        type:Boolean,
+        default: false,
+    },
+    friends:[
+        {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        },
+    ],
+}, 
+{timestamps:true});
 //createdAt, updatedAt
 
 //member since createdAt
+
+const User = mongoose.model("User", userSchema);
+//pre hook
+//It is called as password hashing where we will just turn our password to some gibbrish unreadbale text or something 
+//E.g. john@email.com 12345 => $%^&976%^
+
+userSchema.pre("save", async function(next)
+{
+
+    if(!this.isModified("password")) return next();
+    try{
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+
+        next();
+    }catch(error){
+        next(error);
+    }
+    
+})
+export default User;

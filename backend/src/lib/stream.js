b48a7fe0c3c76b232b9a0b0d9e88d40e29973c56
manshisingh789 <manshisingh353@gmail.com>
2025-06-11 12:36:38 +0530

    done backend

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,8 +1,8 @@
 import { StreamChat } from "stream-chat";
 import "dotenv/config";
 
-const apiKey = process.env.STEAM_API_KEY;
-const apiSecret = process.env.STEAM_API_SECRET;
+const apiKey = process.env.CONNECT_API_KEY;
+const apiSecret = process.env.CONNECT_API_SECRET;
 
 if (!apiKey || !apiSecret) {
   console.error("Stream API key or Secret is missing");
@@ -21,11 +21,9 @@ export const upsertStreamUser = async (userData) => {
 
 export const generateStreamToken = (userId) => {
   try {
-    // ensure userId is a string
     const userIdStr = userId.toString();
     return streamClient.createToken(userIdStr);
   } catch (error) {
     console.error("Error generating Stream token:", error);
   }
 };
- 
